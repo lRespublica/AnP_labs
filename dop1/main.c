a8b2c77449423f7be6575630e3f4772ff199ef57
Leonid Znamenok <respublica@altlinux.org>
2023-10-17 18:04:41 +0300

    Done dop1

--- dop1/main.c
+++ dop1/main.c
@@ -22,8 +22,8 @@ int mpow (int a, unsigned int n) {
     return res;
 }
 
-int mpowd (double a, unsigned int n) {
-    int res = 1;
+double mpowd (double a, unsigned int n) {
+    double res = 1;
     while(n)
     {
         if (n & 1)
@@ -64,10 +64,18 @@ int mround(double x)
         return mfreexp(x) >= 0.5 ? (int)x - 1 : (int)x;
 }
 
+int mtrunc(double x)
+{
+    if(x > 0)
+        return (int)x;
+    else
+        return (int)(x + mfreexp(x));
+}
+
 unsigned int mlog2(unsigned int a)
 {
     unsigned int value = 0;
-    while(a)
+    while(a>1)
     {
         value++;
         a >>= 1;
@@ -75,15 +83,35 @@ unsigned int mlog2(unsigned int a)
     return value;
 }
 
-double mlogn(int a, int b)
+double mlogn(unsigned int a, unsigned int b)
 {
-    return (double)mlog2(a)/mlog2(b);
+    return (double)mlog2(a)/(double)mlog2(b);
 }
 
 int main()
 {
     int a, b, c;
-    scanf("%d%d", &a, &b, &c);
-    int res = (int)(mpowd(mlogn((int)(msqrt((double)a/b)), c), b));
-    printf("%d, %d, %d, %d", mround(1.23), mround(1.6), mround(-1.23), mround(-1.6));
+    if (scanf("%d%d%d", &a, &b, &c) != 3)
+    {
+        printf("Invalid input\n");
+        return 1;
+    }
+    double step1 = (double)a/b;
+    double step2 = msqrt(step1);
+    if(step2 != step2)
+    {
+        printf("Failed to calculate square root");
+        return 1;
+    }
+    int step3 = mtrunc(step2);
+    double step4 = mlogn(step3, c);
+    if(step4 != step4)
+    {
+        printf("Failed to calculate log");
+        return 1;
+    }
+    double step5 = mpowd(step4, b);
+    double step6 = mround(step5);
+
+    printf("%lf, %lf, %d, %lf, %lf, %lf", step1, step2, step3, step4, step5, step6);
 }
