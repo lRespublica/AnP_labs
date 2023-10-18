d87d76764cc7400fc04bfbd2cdcaf340d3a761d1
Leonid Znamenok <respublica@altlinux.org>
2023-10-18 17:26:35 +0300

    Corrected form of square in dop2

--- dop2/main.c
+++ dop2/main.c
@@ -2,7 +2,6 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio_ext.h>
-#include <unistd.h>
 
 #define CHECK_INPUT(flag) if (!flag || feof(stdin)) {printf("Incorrect input\n"); return 1;};
 
@@ -36,10 +35,19 @@ int main()
         {
             for(int j = 0; j < x + size; j++)
             {
-                if(j < x)
-                    printf(" ");
+                if(j >= x)
+                {
+                    int fn1 = x;
+                    int fn2 = x + size - 1;
+                    int fn3 = y;
+                    int fn4 = y + size - 1;
+                    if(j == fn1 || j == fn2 || i == fn3 || i == fn4)
+                        printf("*");
+                    else
+                        printf(" ");
+                }
                 else
-                    printf("*");
+                    printf(" ");
             }
         }
         printf("\n");
@@ -49,6 +57,7 @@ int main()
     system("clear");
 
     angel = M_PI*angel/180;
+    // ySize - высота. xSize - разность координат оснований
     int ySize = round(sin(angel) * size);
     int xSize = round(cos(angel) * size);
     for(int i = 0; i < y + ySize; i++)
@@ -61,6 +70,7 @@ int main()
                 int fn2 = y + round(tan(angel) * (j - x));
                 int fn3 = y + ySize - 1;
                 int fn4 = y;
+                // Доп. условия для fn3 и fn4 предназначены для того, чтобы верхняя и нижния линии не выходили за границы
                 if( i == fn1 || i == fn2 || (i == fn3 && j >= x + xSize) || (i == fn4 && j <= x + size && j >= x))
                     printf("*");
                 else
