fe8670f47ca49dd570043b8a4e38c513f64c165a
Leonid Znamenok <respublica@altlinux.org>
2023-12-11 16:31:53 +0300

    Done lab5

--- lab5/main.c
+++ lab5/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <float.h>
 #include <limits.h>
+#include <time.h>
 
 #define CHECK_INPUT(flag) if (!flag || feof(stdin)) {printf("Incorrect input\n"); return 1;};
 
@@ -15,6 +16,15 @@ int inputArray(int** arr, int m, int n)
     }
     
     return 0;
+}
+
+void randomArray(int** arr, int m, int n)
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+            arr[i][j] = ((rand()%32768) - ((RAND_MAX)%32768)/2);
+    }
 }
 
 void printArray(int** arr, int m, int n)
@@ -33,7 +43,7 @@ int findUpperMax(int** arr, int m, int n)
     int max = INT_MIN;
     for(int i = 0; i < m; i++)
     {
-        for(int j = i; j < n; j++)
+        for(int j = i+1; j < n; j++)
         {
             if(arr[i][j] > max)
                 max = arr[i][j];
@@ -47,7 +57,7 @@ int findLowerMin(int** arr, int m, int n)
     int min = INT_MAX;
     for(int i = 0; i < m; i++)
     {
-        for(int j = i; j > 0; j--)
+        for(int j = i-1; j >= 0; j--)
         {
             if(arr[i][j] < min)
                 min = arr[i][j];
@@ -63,6 +73,7 @@ int solveTask(int** arr, int m, int n)
 
 int main()
 {
+    srand(time(NULL));
     int m, n;
 
     printf("Введите размер матрицы\n");
@@ -78,9 +89,13 @@ int main()
         arr[i] = alloca(sizeof(int) * n);
     }
 
-    if(inputArray(arr, m, n) == 1)
-        return 1;
+    //if(inputArray(arr, m, n) == 1)
+    //    return 1;
+    randomArray(arr, m, n);
+    printArray(arr, m, n);
 
     int value = solveTask(arr, m, n);
     printf("\nРезультат: %d\n", solveTask(arr, m, n));
+    printf("\nМаксимальное значение: %d", findUpperMax(arr, m, n));
+    printf("\nМинимальное значение: %d", findLowerMin(arr, m, n));
 }
