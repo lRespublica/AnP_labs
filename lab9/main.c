69db3d43fb1da9efc91cf8db78e6e2269490b7ca
Leonid Znamenok <respublica@altlinux.org>
2024-06-09 21:30:57 +0300

    Added bubble sort in lab9

--- lab9/main.c
+++ lab9/main.c
@@ -67,6 +67,47 @@ void printRecord(const record* record, FILE* dest)
     fprintf(dest, "Baggage: %d\n", record->baggage);
 }
 
+void printRecordsArray(record** records, int amount, FILE* dest)
+{
+    for(int i = 0; i < amount; i++)
+    {
+        fprintf(dest, "Record %d\n", i);
+        printRecord(records[i], dest);
+        fprintf(dest, "\n");
+    }   
+}
+
+record** clearRecordsArray(record** records, int amount)
+{
+    for(int i = 0; i < amount; i++)
+        free(records[i]);
+
+    free(records);
+    return NULL;
+}
+
+record** copyRecordsArray(record** records, int amount)
+{
+    record** genRecords = malloc(sizeof(record*) * amount);
+
+    for(int i = 0; i < amount; i++)
+    {
+        genRecords[i] = malloc(sizeof(record));
+
+        genRecords[i]->flight = records[i]->flight;
+        genRecords[i]->baggage = records[i]->baggage;
+        genRecords[i]->seat = records[i]->seat;
+
+        genRecords[i]->date.day = records[i]->date.day;
+        genRecords[i]->date.year = records[i]->date.year;
+        genRecords[i]->date.month = records[i]->date.month;
+
+        strncpy(genRecords[i]->name, records[i]->name, 32);
+    }
+
+    return genRecords;
+}
+
 int compareRecord(const record* record1, const record* record2, sortOrder order)
 {
     int result = 0;
@@ -133,7 +174,7 @@ int compareRecord(const record* record1, const record* record2, sortOrder order)
         equalFlag = 1;
     }
 
-    if(order == DESCENDING)
+    if(order == DESCENDING && equalFlag == 0)
         result = !result;
     else if(order == EQUAL)
         result = equalFlag;
@@ -147,7 +188,7 @@ int compareRecord(const record* record1, const record* record2, sortOrder order)
     return result;
 }
 
-record* generateRandomRecords(int amount)
+record** generateRandomRecords(int amount)
 {
     if(DEBUG)
     {
@@ -155,35 +196,37 @@ record* generateRandomRecords(int amount)
         fprintf(stderr, "Begin of generation\n\n");
     }
 
-    record* randRecords = malloc(sizeof(record) * amount);
+    record** randRecords = malloc(sizeof(record*) * amount);
 
     for(int i = 0; i < amount; i++)
     {
-        randRecords[i].flight = rand() % 65536;
-    
-        randRecords[i].date.year = (rand() % 115) + 1909;
-        randRecords[i].date.month = (rand() % 12) + 1;
-        randRecords[i].date.day = (rand() % 28) + 1;
+        randRecords[i] = malloc(sizeof(record));
 
-        randRecords[i].seat = rand() % 255;
-        randRecords[i].baggage = rand() % 255;
+        randRecords[i]->flight = rand() % 65536;
+    
+        randRecords[i]->date.year = (rand() % 115) + 1909;
+        randRecords[i]->date.month = (rand() % 12) + 1;
+        randRecords[i]->date.day = (rand() % 28) + 1;
+
+        randRecords[i]->seat = rand() % 255;
+        randRecords[i]->baggage = rand() % 255;
 
         int nameLen[2] = {rand() % 15 + 1, rand() % 15 + 1};
 
         for(int j = 0; j < nameLen[0] + nameLen[1] + 1; j++)
         {
             if (j == 0 || j == nameLen[0] + 1)
-                randRecords[i].name[j] = (rand() % 26) + 65;
+                randRecords[i]->name[j] = (rand() % 26) + 65;
             else if(j == nameLen[0])
-                randRecords[i].name[j] = ' ';
+                randRecords[i]->name[j] = ' ';
             else
-                randRecords[i].name[j] = (rand() % 26) + 97;
+                randRecords[i]->name[j] = (rand() % 26) + 97;
         }
 
         if(DEBUG)
         {
             fprintf(stderr, "Record %d\n", i);
-            printRecord(&randRecords[i], stderr);
+            printRecord(randRecords[i], stderr);
             fprintf(stderr, "\n\n");
         }
     }
@@ -197,17 +240,57 @@ record* generateRandomRecords(int amount)
     return randRecords;
 }
 
+int isSorted(record** records, int amount, sortOrder order)
+{
+    int isSorted = 1;
+
+    for(int i = 0; i < amount - 1; i++)
+    {
+        if(compareRecord(records[i], records[i+1], order) == 1)
+        {
+            isSorted = 0;
+            break;
+        }
+    }
+
+    return isSorted;
+}
+
+void bubbleSort(record** records, int amount, sortOrder order)
+{
+    for(int i = amount; i >= 0; i--)
+    {
+        int continueFlag = 0;
+        for(int j = 1; j < i; j++)
+        {
+            if(compareRecord(records[j - 1], records[j], order) == 1)
+            {
+                record* tmp = records[j];
+                records[j] = records[j - 1];
+                records[j-1] = tmp;
+
+                continueFlag = 1;
+            }
+        }
+        if(continueFlag == 0)
+            break;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
 
     int amount = 10;
 
-    record* generatedRecords = generateRandomRecords(amount);
+    sortOrder order = DESCENDING;
 
-    date tmpDate1 = {2010, 4, 4}; 
-    date tmpDate2 = {2012, 3, 5}; 
-    
-    record tmpRecord1 = {10, tmpDate1, "Mr. Mark Knopfler", 11, 32};
-    record tmpRecord2 = {10, tmpDate1, "Mrs. Misery", 10, 2};
+    record** generatedRecords = generateRandomRecords(amount);
+
+    record** toBeBubbleSorted = copyRecordsArray(generatedRecords, amount);
+
+    bubbleSort(toBeBubbleSorted, amount, order);
+
+    printRecordsArray(generatedRecords, 10, stderr);
+    printRecordsArray(toBeBubbleSorted, 10, stderr);
 }
